7b5afcace2ea15ea2a61c50ff36595e45e81fd92
Grégoire Pineau <lyrixx@lyrixx.info>
2018-06-13 17:11:59 +0200

    bootstrap, entity type

--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -2,8 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Author;
 use App\Entity\Book;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +15,13 @@ class BookType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('author')
+            ->add('title', TextType::class)
+            ->add('author', EntityType::class, [
+                'class' => Author::class,
+                'choice_label' => function (Author $author) {
+                    return $author->getName();
+                },
+            ])
         ;
     }
 
